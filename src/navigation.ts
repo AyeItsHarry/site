61b0dab3766b17dd585a89edb9007fff3c55980f
Harry Walker <me@harrywalker.uk>
2024-11-29 21:25:57 +0000

    Add social links, update dependencies, and move blog posts into blog subfolder

--- src/navigation.ts
+++ src/navigation.ts
@@ -16,11 +16,13 @@ export const headerData = {
 
 export const footerData = {
   socialLinks: [
+    { ariaLabel: 'YouTube', icon: 'tabler:brand-youtube', href: 'https://youtube.com/@HarryWalker-UK' },
+    { ariaLabel: 'Twitch', icon: 'tabler:brand-twitch', href: 'https://twitch.tv/harrywalkeruk' },
     { ariaLabel: 'Bluesky', icon: 'tabler:brand-bluesky', href: 'https://bsky.app/profile/harrywalker.uk' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/ayeitsharry' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/harrywalkeruk' },
     { ariaLabel: 'Discord', icon: 'tabler:brand-discord', href: 'https://discord.gg/rreHJdkfQh' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/ayeitsharry' },
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
